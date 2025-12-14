4910d915bed7d59333967e8cfb710bd5ac108ad5
JavaVoidWasTaken <brahm.wn@gmail.com>
2025-12-14 21:00:05 +0700

    Attempting to solve a USACO challenge.

--- Cpp/CodingCompetitionPractice/raw.cpp
+++ Cpp/CodingCompetitionPractice/raw.cpp
@@ -6,58 +6,14 @@
 using namespace std;
 using ll = long long;
 
-bool isCharInsideIndexOfStrings(int index, char search, vector<string> list){
-	for (int i = 0; i < list.size(); ++i){
-		if (list[i][index] == search){
-			return true;
-		}
-	}
 
-	return false;
-}
 
 int main(){
-	/*
-	freopen("cownomics.in", "r", stdin);
-	freopen("cownomics.out", "w", stdout);
-	*/
-
-	int cowAmount, genomeSize, result = 0;
-	bool isGenomeUnique;
-	string temp;
-	string genomeCase = "ATCG";
-	vector<string> spottyList, plainList;
-
-	cin >> cowAmount >> genomeSize;
-
-	for (int i = 0; i < cowAmount; ++i){
-		cin >> temp;
-		spottyList.push_back(temp);
+	int result;
+	string cows;
+	cin >> cows;
+	
+	for (int i = 0; i < 26; ++i){
+		
 	}
-	for (int i = 0; i < cowAmount; ++i){
-		cin >> temp;
-		plainList.push_back(temp);
-	}
-
-	for (int i = 0; i < cowAmount; ++i){
-		isGenomeUnique = true;
-		int j = 0;
-		while (j < 4){
-			if (isCharInsideIndexOfStrings(i, genomeCase[j], spottyList)){
-				if (isCharInsideIndexOfStrings(i, genomeCase[j], plainList)){
-					cout << genomeCase[j] << " ";
-					isGenomeUnique = false;
-					break;
-				} 
-			}
-			++j;   
-		}
-
-		if (isGenomeUnique){
-			result++;
-		}
-	}
-
-	cout << result << '\n';
-
 }
