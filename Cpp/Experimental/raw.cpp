749940d8dc9ec2be1bab9040f0875951d8468434
JavaVoidWasTaken <brahm.wn@gmail.com>
2025-12-15 22:42:04 +0700

    Cleaned up Cpp/Experimental, moved the Text Expander Miniproject to Cpp/MiniProjects.

--- Cpp/Experimental/raw.cpp
+++ Cpp/Experimental/raw.cpp
@@ -4,37 +4,8 @@
 #include <vector>
 #include <string>
 using namespace std;
-using ll = long long;
-
-class text{
-public:
-	int lengthMultiplier, heightMultiplier;
-	vector<string> text;
-};
+using ll = long long;;
 
 int main(){
-
-	text thing;
-	string temp;
-
-	cin >> thing.lengthMultiplier >>  thing.heightMultiplier;
-
-	while (cin){
-		cin >> temp;
-		thing.text.push_back(temp);
-	}
-
-	for (int i = 0; i < thing.text.size()-1; ++i){
-		for (int j = 0; j < thing.heightMultiplier; ++j){
-			for (int k = 0; k < thing.text[i].length(); ++k){
-				for (int l = 0; l < thing.lengthMultiplier; ++l){
-					cout << thing.text[i][k];
-				}
-			}
-			cout << '\n';
-		}
-	}
-
-	cout << "\nProcess done!\n"; 
 
 }
---
+++ Cpp/MiniProjects/TextExpander/sh/makeBackup.sh
@@ -0,0 +1,4 @@
+echo Please type in your desired backup ID:
+read backupID
+
+cp ./raw.cpp ./Backups/backup$backupID.cpp
