ccd136694cddeb550a06233b3b6a62f44e0c2b99
JavaVoidWasTaken <brahm.wn@gmail.com>
2026-05-11 14:35:09 +0700

    Worked on another leetcode problem (Valid Parenthesis)

---
+++ Cpp/Leetcode/validParenthesis.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char> stack = {};
+
+      for (char bracket : s) {
+        if (bracket == '(' || bracket == '{' || bracket == '[') {
+          stack.push(bracket);
+        } else {
+          if (stack.empty()) {
+            return false;
+          }
+          char top = stack.top();
+          stack.pop();
+          if (top == '(' && bracket != ')') return false;
+          if (top == '{' && bracket != '}') return false;
+          if (top == '[' && bracket != ']') return false;
+        }
+      }
+      
+      return stack.empty();
+    }
+};
+
