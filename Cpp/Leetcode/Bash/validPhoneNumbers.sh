5ec955984a1b2b2e4bb3ab166ceca24b7cc5ea06
JavaVoidWasTaken <brahm.wn@gmail.com>
2026-05-19 17:11:15 +0700

    Finished a LeetCode Problem: Remove Duplicates from Sorted List

--- Cpp/Leetcode/Bash/validPhoneNumbers.sh
+++
@@ -1,2 +0,0 @@
-# Leetcode scammed me, the output is the same as the solution but it REJECTED my answer.
-grep -E '^((\([0-9]{3}\) )|([0-9]{3}-))[0-9]{3}-[0-9]{4}' file.txt
---
+++ Cpp/Leetcode/sqrtx.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left = 0;
+        int right = x/2;
+        while ()
+        return mult;
+    }
+};
