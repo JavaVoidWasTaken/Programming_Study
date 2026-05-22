b13d0247eb24fe347750878721b553efaede3da2
JavaVoidWasTaken <brahm.wn@gmail.com>
2026-05-22 18:41:46 +0700

    Completed a leetcode problem: sqrt(x)

--- Cpp/Leetcode/sqrtx.cpp
+++ Cpp/Leetcode/sqrtx.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int left = 0;
-        int right = x/2;
-        while ()
-        return mult;
+        long mult = 1;
+        while (mult*mult*2 < x) {
+          mult *= 2;
+        }
+        while (mult*mult < x) {
+          mult += 1;
+        }
+        while (mult*mult > x) {
+          mult -= 1;
+        }
+        return (int)mult;
     }
 };
